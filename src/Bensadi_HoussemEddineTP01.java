84d52814297e9641dd2e33371841fdcd259dab6c
wawadz2000 <wawadz2000@gmail.com>
2021-11-24 15:24:03 +0100

    first commit

---
+++ src/Bensadi_HoussemEddineTP01.java
@@ -0,0 +1,53 @@
+import java.util.concurrent.Semaphore;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+ public class Bensadi_HoussemEddineTP01 {
+    static Semaphore A,B;
+    static int na , nb;
+ public static class PA extends Thread {
+    @Override
+    public void run() {
+        for (int i = 0; i < 100; i++) {
+            try {
+                A.acquire();
+                na++;
+                System.out.println("na= "+na);
+                A.release();
+            } 
+            catch (InterruptedException e) {
+                Logger.getLogger(Bensadi_HoussemEddineTP01.class.getName()).log(Level.SEVERE, null, e);
+            }
+        }
+    }
+    }
+    
+     public static class PB extends Thread {
+        @Override
+        public void run() {
+            for (int i = 0; i < 100; i++) {
+                try {
+                    B.acquire();
+                     nb++;
+                System.out.println("nb= "+nb);
+                B.release();
+                    
+                } catch 
+                (InterruptedException e) {
+                    Logger.getLogger(Bensadi_HoussemEddineTP01.class.getName()).log(Level.SEVERE, null, e);
+                }
+            }
+    }
+}
+    
+    public static void main(String[] args) {
+
+        na=nb=0;
+        B=new Semaphore(0);
+        A=new Semaphore(10);
+         PA pa = new PA();
+         pa.start();
+         PB pb = new PB();
+         pb.start();
+        };
+    };
